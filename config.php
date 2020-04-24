e8cfb34a0c978888ff551d62cedd3593c5265f4e
bharat11 <contact2bharat@gmail.com>
2020-04-24 13:01:13 +0530

    config commit

---
+++ config.php
@@ -0,0 +1,21 @@
+<?php
+/*
+Organization: SPRWORK
+Date: 24-04-2020
+Developer Name: Bharat
+Developer Code: IT-GEEK#01
+*/
+
+/* Host  Name */
+$SETTINGS["hostname"]='localhost';
+/* Database  User */
+$SETTINGS["mysql_user"]='Please Change Accordingly';
+/* Database  Password */
+$SETTINGS["mysql_pass"]='Please Change Accordingly';
+/* Database  Name */
+$SETTINGS["mysql_database"]='Please Change Accordingly';
+/* Registration Table */
+$SETTINGS["data_table"]='registrations';
+/* Paypal Email Address */
+$SETTINGS["paypal_address"]='testl@payal.com';
+?>
